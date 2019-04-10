37255095db18cc049696deed9fe5a4bbce2306cc
Edward Zhou <edward.zhq@gmail.com>
2019-04-10 15:22:58 +0800

    update spec

--- lib/authable/auth_accounts.ex
+++ lib/authable/auth_accounts.ex
@@ -3,17 +3,17 @@ defmodule Authable.AuthAccounts do
   @doc """
 
   """
-  @callback find_user(user_id :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_user(user_id :: bitstring()) :: any() | nil
 
-  @callback find_user_by_email(email :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_user_by_email(email :: bitstring()) :: any() | nil
 
-  @callback find_app(app_id :: bitstring(), user_id :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_app(app_id :: bitstring(), user_id :: bitstring()) :: any() | nil
 
-  @callback find_client(client_id :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_client(client_id :: bitstring()) :: any() | nil
 
-  @callback find_client(client_id :: bitstring(), redirect_uri :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_client(client_id :: bitstring(), redirect_uri :: bitstring()) :: any() | nil
 
-  @callback find_token(token_name :: bitstring(), token_value :: bitstring()) :: {:ok, any()} | {:error, term()}
+  @callback find_token(token_name :: bitstring(), token_value :: bitstring()) :: any() | nil
 
   def find_user(auth_accounts_impl, user_id) do
     auth_accounts_impl.find_user(user_id)
